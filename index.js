3a53c910af819d121b88d0989b7f90b72320ada8
LinaTrueCat <146427251+LinaTrueCat@users.noreply.github.com>
2023-10-06 18:31:01 +0300

    added all from sandbox

---
+++ index.js
@@ -0,0 +1,22 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80
+  },
+  tokyo: {
+    temp: 17,
+    humidity: 70
+  },
+  lisbon: {
+    temp: 30,
+    humidity: 20
+  },
+  Melbourne: {
+    temp: 20.9,
+    humidity: 100
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20
+  }
+};
